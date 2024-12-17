20133cefd4ef1e20fd3cf9c99dc85b4e647a9662
OmarRMC <mamani.capcha.omarrodrigo@gmail.com>
2024-12-17 12:03:42 -0400

    Configuraciones de ReactTS y Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Proyecto de Todo-React TS 
+    </h1>
+      <button className="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">Prueba</button>
+    </>
+  )
+}
+
+export default App
