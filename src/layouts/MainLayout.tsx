f8b03cbc881445e76a82fd2aa421ac054a7798a9
OmarRMC <mamani.capcha.omarrodrigo@gmail.com>
2024-12-17 22:40:52 -0400

    Se agrego el diseño de dasboard

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,16 @@
+import Footer from "../components/layout/Footer";
+import Header from "../components/layout/Header";
+
+function MainLayout() {
+    return (
+        <>
+            <Header />
+            <main>
+                <p>Main</p>
+            </main>
+            <Footer />
+        </>
+    );
+}
+
+export default MainLayout;
